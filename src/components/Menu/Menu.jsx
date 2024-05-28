b8be77bf9fd98ecfa58f0e4f140ad8a5de12aae7
GunicDev <gunic.dev@gmail.com>
2024-05-28 14:28:12 +0200

    menu done

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -40,7 +40,7 @@ export default function NavigationMenu() {
                             to={item.href}
                             className={classNames(
                               item.href === location.pathname
-                                ? "bg-gray-900 text-white"
+                                ? "bg-gray-700 text-white"
                                 : "text-gray-300 hover:bg-gray-700 hover:text-white",
                               "rounded-md px-3 py-2 text-sm font-medium"
                             )}
@@ -89,11 +89,9 @@ export default function NavigationMenu() {
                         item.current
                           ? "bg-gray-900 text-white"
                           : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "rounded-md px-3 py-2 text-sm font-medium"
+                        "block rounded-md px-3 py-2 text-base font-medium"
                       )}
-                      aria-current={
-                        location.pathname === item.href ? "page" : undefined
-                      }
+                      aria-current={item.current ? "page" : undefined}
                     >
                       {item.name}
                     </DisclosureButton>
