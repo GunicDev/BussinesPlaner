26aba1de94c19ff65c3a88ec9dcc033b6606db14
GunicDev <gunic.dev@gmail.com>
2024-05-30 08:29:05 +0200

    UI fixed

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -2,7 +2,7 @@ import DayDetail from "../components/Days/DayDetail";
 
 export default function DetailPage() {
   return (
-    <div className="p-3 mt-3">
+    <div className="p-3 mt-3 lg:mr-32 lg:ml-32">
       <DayDetail />
     </div>
   );
