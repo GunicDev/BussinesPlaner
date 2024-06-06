86e01028f20fb1be907173dd1e8c9691d9e443ab
GunicDev <gunic.dev@gmail.com>
2024-06-06 12:52:57 +0200

    fixed

--- src/components/Days/NewDay.jsx
+++ src/components/Days/NewDay.jsx
@@ -11,17 +11,28 @@ export default function NewDay() {
   const navigate = useNavigate();
 
   const [inputValue, setInputValue] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
 
   const newDayHandler = (event) => {
     setInputValue(event.target.value);
+    setShowMessage(false);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
+    if (inputValue.length === 0 || inputValue.trim() === "") {
+      setShowMessage(true);
+      return;
+    }
 
     dispatch(newDay(fbDays, inputValue));
+    setShowMessage(false);
 
     navigate("/home");
+  };
+
+  const backHandler = () => {
+    navigate(-1);
   };
 
   return (
@@ -29,12 +40,29 @@ export default function NewDay() {
       <form className="lg:w-2/3" onSubmit={submitHandler}>
         <div className="p-3 m-2">
           <Input label={"Name"} onChange={(event) => newDayHandler(event)} />
+          {showMessage && <p className="text-red-400 mt-1">Required field</p>}
         </div>
 
         <div className="p-3 m-2">
-          <Button type={"submit"}>Add Day</Button>
+          <Button
+            type={"submit"}
+            bgColor={"bg-green-200"}
+            textColor={"text-white"}
+            textHover={"text-green-600"}
+          >
+            Add Day
+          </Button>
         </div>
       </form>
+      <div className="text-right">
+        <Button
+          bgColor={"none"}
+          textColor={"text-red-200"}
+          onClick={backHandler}
+        >
+          Back
+        </Button>
+      </div>
     </>
   );
 }
